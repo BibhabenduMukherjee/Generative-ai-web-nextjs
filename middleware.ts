5b42eef4d957542cabe381ea0e9233bee966e9ee
= <mukherjee4004@gmail.com>
2023-07-24 23:37:00 +0530

    setup auth

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware();
+// specify which one should be public 
+export default authMiddleware({
+    publicRoutes : ["/"]
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
