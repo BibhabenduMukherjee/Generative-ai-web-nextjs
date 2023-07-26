540eab958bd3d902db83298145076de12cb8af02
= <mukherjee4004@gmail.com>
2023-07-27 00:00:08 +0530

    desing dashboard 2

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,12 +2,11 @@ import React from 'react'
 import { Button } from './ui/button'
 import {Menu} from "lucide-react"
 import { UserButton } from '@clerk/nextjs'
+import  {MobileSidebar}  from './mobile-sidebar'
 function Navbar() {
   return (
-    <div className = 'flex items-center p-4'>
-        <Button variant={"ghost"} size = "icon" className="md:hidden" >
-            <Menu/>
-        </Button>
+    <div className = 'flex items-center p-2'>
+        <MobileSidebar />
         <div className = 'flex  w-full justify-end'>
             <UserButton afterSignOutUrl='/'/>
         </div>
